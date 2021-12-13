8d4510e6868691d9abfe2ecf42bc238bed9a126e
unknown <jhankus@student.agh.edu.pl>
2021-12-13 19:38:29 +0100

    loading States extended to handle errors

--- mobile/App.jsx
+++ mobile/App.jsx
@@ -2,24 +2,27 @@ import { StatusBar } from 'expo-status-bar';
 import React, { Component } from 'react';
 import { AppRegistry, StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
+
+
 export default class App extends Component {
 
     constructor() {
         super();
+
         this.state = {
             ip: '',
             fetched_data: [],
-            isLoading: false,
+            loadingState: 1
         }
         
     }
     
     handlePress() {
-        let examples = this.getArticles();
+        this.getArticles();
     };
 
     exit() {
-        this.setState({ isLoading:false })
+        this.setState({ loadingState: 1 })
     }
 
     async getArticles() {
@@ -27,15 +30,18 @@ export default class App extends Component {
             .then(response => response.json())
             .then(data => {
                 console.log(data);
-                this.setState({ isLoading: true, fetched_data: data })
+                this.setState({ loadingState: 2, fetched_data: data })
             })
-            .catch(err => console.error(err));
+            .catch(err => {
+                console.error(err);
+                this.setState({ loadingState: 3 })
+            });
 
     }
 
 
     render() {
-        if (!this.state.isLoading) {
+        if (this.state.loadingState === 1) {
             return (<View style={styles.container}>
                 <Text></Text>
                 <Text></Text>
@@ -49,16 +55,26 @@ export default class App extends Component {
             </View>);
         }
 
-        else {
+        else if (this.state.loadingState === 2) {
             return (
                 //warning: for now showing fetched_data is hardcoded, this is for change when json keys specified
                 <View style={styles.container}>
-                <Text>{this.state.fetched_data.date}</Text>
-                <Text></Text>
-                <Button title="<" onPress={this.exit.bind(this)} />
-            </View>
+                    <Text>{this.state.fetched_data.date}</Text>
+                    <Text></Text>
+                    <Button title="<" onPress={this.exit.bind(this)} />
+                </View>
             );
-        }             
+        }
+        else {
+            return (
+                <View style={styles.container}>
+                    <Text>Sorry, something went wrong, try again</Text>
+                    <Text></Text>
+                    <Button title="<" onPress={this.exit.bind(this)} />
+                </View>
+
+            );
+        }
    
     }
 }
