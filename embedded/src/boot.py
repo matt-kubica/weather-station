c834c0d3fa3b5edbcd4cb84f76ec01f67839b90b
qbic22562 <mateusz.kubica99@gmail.com>
2021-12-14 11:55:08 +0100

    Adds AP mode to embedded

--- embedded/src/boot.py
+++ embedded/src/boot.py
@@ -9,17 +9,29 @@ gc.collect()
 
 import network
 from time import sleep
-SSID = 'Genesis'
+
+# to configure
+WLAN_MODE = network.AP_IF
+ESSID = 'ESP8266'
+SSID = 'iPhone (Mateusz)'
 PASS = 'ojciecdyrektor'
 
-station = network.WLAN(network.STA_IF)
+print('WLAN configured to {0}'.format(WLAN_MODE))
+wlan = network.WLAN(WLAN_MODE)
+wlan.active(True)
 
-station.active(True)
-station.connect(SSID, PASS)
+if WLAN_MODE == network.STA_IF:
+	wlan.connect(SSID, PASS)
+	print('\n\n\n\nConnecting to {0}...'.format(SSID), end='')
+	while wlan.isconnected() == False:
+		print('...', end='')
+		sleep(1)
+	print('successful!')
 
-print('\n\n\n\n')
-print('Connecting to {0}...'.format(SSID), end='')
-while station.isconnected() == False:
-	print('...', end='')
-	sleep(1)
-print('successful!')
+elif WLAN_MODE == network.AP_IF:
+	wlan.config(essid=ESSID, authmode=network.AUTH_WPA_WPA2_PSK, password=PASS)
+	print('\n\n\n\nSetting up access point...', end='')
+	while wlan.active() == False:
+		print('...', end='')
+		sleep(1)
+	print('successful!')
--- embedded/src/main.py
+++ embedded/src/main.py
@@ -2,7 +2,7 @@ from time import sleep
 import ujson as json
 import usocket as socket
 
-from boot import station
+from boot import wlan
 from machine import Pin, I2C
 from bme import BME280
 i2c = I2C(scl=Pin(5), sda=Pin(4), freq=10000)
@@ -13,7 +13,7 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('', 80))
 s.listen(5)
 
-print('--- ESP8266 Micropython Wheather Station on {0} ---'.format(station.ifconfig()[0]))
+print('--- ESP8266 Micropython Wheather Station on {0} ---'.format(wlan.ifconfig()[0]))
 while True:
 	conn, addr = s.accept()
 	request = conn.recv(1024)
@@ -25,7 +25,7 @@ while True:
 	  'pressure': bme.pressure
   	}
 
-	print('-- DATA -----------------------------------')
+	print('-- DATA -------------------------------------------------')
 	print('\tTemperature: {0}'.format(data['temperature']))
 	print('\tHumidity: {0}'.format(data['humidity']))
 	print('\tPressure: {0}'.format(data['pressure']))
